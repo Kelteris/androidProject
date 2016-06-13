2e8138536afb30c6f89bf5f9de97e2693db43d22
TravisCon <confer.travis@gmail.com>
2016-06-13 10:06:19 -0600

    Keyboard Works!

--- RM_Planner/app/src/main/java/com/cs246/rmgroup/rmplanner/MainActivity.java
+++ RM_Planner/app/src/main/java/com/cs246/rmgroup/rmplanner/MainActivity.java
@@ -31,6 +31,11 @@ import java.util.Calendar;
 public class MainActivity extends AppCompatActivity {
     ArrayList<String> list = new ArrayList<>();
     int[] hours = {7, 8, 9, 10, 11, 12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12};
+    String[] strHours = {"7:00", "8:00", "9:00", "9:30", "10:00", "10:30",
+            "11:00", "11:30", "12:00", "12:30", "1:00", "1:30", "2:00", "2:30",
+            "3:00", "3:30", "4:00", "4:30", "5:00", "5:30", "6:00", "6:30", "7:00",
+            "7:30", "8:00", "8:30", "9:00", "9:30", "10:00", "10:30",
+            "11:00", "11:30", "12:00"};
     ListView listView;
     ArrayAdapter<String> adapter;
     FlyOutContainer root;
@@ -133,19 +138,20 @@ public class MainActivity extends AppCompatActivity {
     void buildPlannerView() {
         //Setup gridLayout
         gLayout.setColumnCount(2);
-        gLayout.setRowCount(hours.length);
+        gLayout.setRowCount(strHours.length);
 
         /************************************
          * Bring in all hours in left column
          ***********************************/
-        for (int i = 0; i < hours.length; i++) {
+        for (int i = 0; i < strHours.length; i++) {
             GridLayout.LayoutParams params = new
                     GridLayout.LayoutParams(GridLayout.spec(i, GridLayout.CENTER),
                     GridLayout.spec(0, GridLayout.CENTER));
             params.setMargins(5, 0, 0, 0);
             params.setGravity(Gravity.FILL);
             TextView tv = new TextView(this);
-            tv.setText(Integer.toString(hours[i]) + ((i < 5) ? "AM" : "PM"));
+            //tv.setText(Integer.toString(hours[i]) + ((i < 5) ? "AM" : "PM"));
+            tv.setText(strHours[i]);
             tv.setTextSize(pixelsToDp(35, this));
             tv.setPadding(10,10,10,10);
             gLayout.addView(tv, params);
@@ -154,7 +160,7 @@ public class MainActivity extends AppCompatActivity {
         /*******************************
          * Bring in all editTexts
          ******************************/
-        for (int i = 0; i < hours.length; i++) {
+        for (int i = 0; i < strHours.length; i++) {
             GridLayout.LayoutParams params = new
                     GridLayout.LayoutParams(GridLayout.spec(i, GridLayout.CENTER),
                     GridLayout.spec(1, GridLayout.LEFT));
@@ -176,6 +182,7 @@ public class MainActivity extends AppCompatActivity {
         for (int i = 0; i < count; i++) {
             View v = gLayout.getChildAt(i);
             v.setBackgroundResource(R.drawable.draw_back_left);
+            //v.setBackgroundColor(Integer.parseInt("ACACAC", 16) + (50 * i));
             if (gLayout.getChildAt(i) instanceof EditText) {
                 EditText e = (EditText) gLayout.getChildAt(i);
                 e.setBackgroundResource(R.drawable.draw_back);
